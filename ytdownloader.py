958391e1814923cf193a90f1af5cbe1cd73d7159
Miguel Arrelias <miguel.p.arrelias@gmail.com>
2022-03-28 00:27:07 -0300

    ajustar

--- ytdownloader.py
+++ ytdownloader.py
@@ -9,9 +9,9 @@ video = list(enumerate(videos))  # lista os formatos, começando do zero
 for i in video:  # vai printar os formatos na lista
     print(i)
 
-print("Escolha o formato que deseja baixar")
+print("\n Escolha o formato que deseja baixar")
 dn_option = int(input("Insira a opção: "))  # opção para escolher o formato
 dn_video = videos[dn_option]
 dn_video.download()  # baixa o video
 
-print("download concluído !!")
+print("download concluído!!")
