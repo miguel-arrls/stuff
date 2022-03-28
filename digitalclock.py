f18574f6e1c846a2020d21e2d5e4b5141011214c
Miguel Arrelias <miguel.p.arrelias@gmail.com>
2022-03-27 22:15:46 -0300

    Youtube Download

--- digitalclock.py
+++ digitalclock.py
@@ -1,14 +1,19 @@
-# importando os modulos
-
 from tkinter import *
 from tkinter.ttk import *
 from time import strftime
 
 root = Tk()
-root.tittle('Clock')
+root.title('Clock')
 
 
 def time():
-    string = strftime('%H:%M:S %p')
+    string = strftime('%H:%M:%S %p')
     lbl.config(text=string)
     lbl.after(1000, time)
+
+
+lbl = Label(root, font=('calibri', 40, 'bold'),
+            background='purple', foreground='white')
+lbl.pack(anchor='center')
+time()
+mainloop()
---
+++ ytdownloader.py
@@ -0,0 +1,17 @@
+from pytube import Youtube
+
+link = input("Insira o link do video do yt url : ")
+yt = Youtube(link)
+videos = yt.streams.all()  # mostra os formatos disponiveis
+
+video = list(enumerate(videos))  # lista os formatos, começando do zero
+
+for i in video:  # vai printar os formatos na lista
+    print(i)
+
+print("Escolha o formato que deseja baixar")
+dn_option = int(input("Insira a opção: "))  # opção para escolher o formato
+dn_video = videos[dn_option]
+dn_video.download()  # baixa o video
+
+print("download concluído !!")
